70630211dc014101d650a02e26a786f66a477d9e
mohitkarnajawala <mohitkaranjawala86@gmail.com>
2022-06-29 11:32:04 +0530

    add log

--- app/src/main/java/com/example/myapplication/receiver/MyReciever.kt
+++ app/src/main/java/com/example/myapplication/receiver/MyReciever.kt
@@ -3,10 +3,12 @@ package com.example.myapplication.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.widget.Toast
 
 class MyReciever : BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
+        Log.e("MyReceiver:",""+p1?.action)
         Toast.makeText(p0, p1?.action.toString(),
             Toast.LENGTH_LONG).show()
     }
